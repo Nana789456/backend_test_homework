c2f3f93ed7b61c424c974cfc8024ea120a8f2692
Nana789456 <gelion-h@yandex.ru>
2026-01-31 23:22:04 +0300

    Переделать все тесты под фикстуры

--- tests/test_get_avarage_value.py
+++ tests/test_get_avarage_value.py
@@ -1,12 +1,19 @@
 from lessons.m_2026_01_18.lesson1 import get_user_with_min_average_value
+import pytest
 
-def test_get_user_with_min_average_value():
-    users_marks = {
-    'user1':[3,1,2],
-    'user2':[5,5,5],
-    'user3':[5,4,5],
-    'user4':[2,1,2],
-}
+
+@pytest.fixture
+def users_marks():
+    """Фикстура с тестовыми данными пользователей и их оценок."""
+    return {
+        'user1': [3, 1, 2],
+        'user2': [5, 5, 5],
+        'user3': [5, 4, 5],
+        'user4': [2, 1, 2],
+        }
+
+
+def test_get_user_with_min_average_value(users_marks):
     expected = 'user4'
     result = get_user_with_min_average_value(users_marks)
     assert expected == result
