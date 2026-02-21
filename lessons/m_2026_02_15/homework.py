83ffc9519cd23e4f3bbdbf7dd295959957937689
Nana789456 <gelion-h@yandex.ru>
2026-02-21 11:17:42 +0300

    Конвертация из Json в dict и обратно

--- lessons/m_2026_02_15/homework.py
+++ lessons/m_2026_02_15/homework.py
@@ -7,3 +7,26 @@
 - Кеширование + декораторы
 - Подготовить вопросы
 """
+
+import json
+
+json_dict = {'game':'футбол', 'age': 21, 'score': None}
+json_str = json.dumps(json_dict, ensure_ascii=True, indent=3)
+print(type(json_str))
+print(json_str[0])
+print(json_str)
+parsed_dict = json.loads(json_str)
+print(type(parsed_dict))
+print(parsed_dict)
+
+json_tuple = (1,2)
+json_str = json.dumps(json_tuple, ensure_ascii=True, indent=3)
+print(type(json_str))
+print(json_str)
+
+parsed_tuple = json.loads(json_str)
+print(type(parsed_tuple))
+print(parsed_tuple)
+python_tuple = tuple(parsed_tuple)
+print(type(python_tuple))
+print(python_tuple)
